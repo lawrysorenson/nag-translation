dded666c932ff1e4c8752e3b36d5917fb3dd7643
Lawry Sorenson <lawrysorenson@gmail.com>
2023-12-04 12:39:40 -0700

    Add masking and pad to length

--- dataset.py
+++ dataset.py
@@ -3,10 +3,45 @@ from torch.utils.data import Dataset, DataLoader
 import os
 from tqdm import tqdm
 import random
+import copy
+import torch
+
+def generate_mask(size, lens):
+  ans = torch.ones((len(lens), size)) # TODO: dtype?
+  for i, l in enumerate(lens):
+    ans[i,l:] = 0
+  return ans
+
+def get_pad_to_longest(PAD):
+    def pad_to_longest(batch):
+        src, tgt_mask, tgt = zip(*batch)
+
+        src_lens = [len(s) for s in src]
+        pad_len = max(src_lens)
+        src_mask = generate_mask(pad_len, src_lens)
+        pad_src = [s + [PAD] * (pad_len - len(s)) for s in src]
+
+        tgt_lens = [len(s) for s in tgt]
+        tgt_mask_lens = [len(s) for s in tgt_mask]
+        pad_len = max(max(tgt_lens), max(tgt_mask_lens))
+        tgt_mask_mask = generate_mask(pad_len, tgt_mask_lens)
+        pad_tgt_mask = [s + [PAD] * (pad_len - len(s)) for s in tgt_mask]
+        pad_tgt = [s + [PAD] * (pad_len - len(s)) for s in tgt]
+
+        pad_src = torch.tensor(pad_src)
+        pad_tgt_mask = torch.tensor(pad_tgt_mask)
+        pad_tgt = torch.tensor(pad_tgt)
+
+        return pad_src, src_mask, pad_tgt_mask, tgt_mask_mask, pad_tgt
+
+    return pad_to_longest
+
 
 class TrainDataset(Dataset):
     def __init__(self, tokenizer = None, data = None, path = './data', train=False):
         self.train = train
+        self.tokenizer = tokenizer
+        self.mask_id = tokenizer.convert_tokens_to_ids(['<mask>'])[0]
         if data is not None:
             self.data = data
         else:
@@ -14,9 +49,15 @@ class TrainDataset(Dataset):
             src_path = os.path.join(path, 'src.txt')
             tgt_path = os.path.join(path, 'tgt.txt')
             with open(src_path, 'r') as src_in, open(tgt_path, 'r') as tgt_in:
+                i = 0
                 for src, tgt in zip(tqdm(src_in.readlines(), desc='Loading dataset'), tgt_in):
+                    i += 1
+                    if i == 1000: break # limit input for fast development
                     src=src.strip()
                     tgt=tgt.strip()
+
+                    src = tokenizer.encode(src)
+                    tgt = tokenizer.encode(tgt)
 
                     self.data.append((src, tgt))
 
@@ -31,9 +72,9 @@ class TrainDataset(Dataset):
         val_dataset = self.data[int(N*train):-int(N*test)]
         test_dataset = self.data[-int(N*test):]
 
-        train_dataset = TrainDataset(data=train_dataset, train=True)
-        val_dataset = TrainDataset(data=val_dataset)
-        test_dataset = TrainDataset(data=test_dataset)
+        train_dataset = TrainDataset(tokenizer=self.tokenizer, data=train_dataset, train=True)
+        val_dataset = TrainDataset(tokenizer=self.tokenizer, data=val_dataset)
+        test_dataset = TrainDataset(tokenizer=self.tokenizer, data=test_dataset)
 
         return train_dataset, val_dataset, test_dataset
 
@@ -41,7 +82,11 @@ class TrainDataset(Dataset):
         self.train = train
 
     def __getitem__(self, i):
-        return self.data[i]
+        src, tgt = self.data[i]
+        tgt_mask = copy.copy(tgt)
+        mask_ind = random.randint(0, len(tgt_mask))
+        tgt_mask[mask_ind] = self.mask_id
+        return src, tgt_mask, tgt
   
     def __len__(self):
         return len(self.data)
